a6953fcf99b113a7d1df71b4af4090ac89e9f206
cclerger <cybill.clerger@telecom-paristech.fr>
2015-06-16 14:22:45 +0200

    Constructeur + getters dans Gate

--- PTM/src/Gate.java
+++ PTM/src/Gate.java
@@ -1,4 +1,40 @@
 
 public abstract class Gate {
-
+	
+	private String output;
+	private String input1;
+	private String input2;
+	private int[][] ptm;
+	
+	public Gate(String output, String input1, String input2) {
+		
+		this.output = output;
+		this.input1 = input1;
+		this.input2 = input2;
+		
+	}
+	
+	public String getOutput() {
+		
+		return output;
+		
+	}
+	
+	public String getInput1() {
+		
+		return intput1;
+		
+	}
+	
+	public String getInput2() {
+		
+		return input;
+		
+	}
+	
+	public String getPtm() {
+		
+		return ptm;
+		
+	}
 }
